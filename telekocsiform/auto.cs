0f707bfb8994fccc61f0044d9c0376386e43271b
lajtosdavid11 <lajtos.david11@gmail.com>
2020-10-22 09:27:55 +0200

    Add project files.

---
+++ telekocsiform/auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telekocsiform
+{
+    class auto
+    {
+        public string Indulas { get; private set; }
+        public string Cel { get; private set; }
+        public string Rendszam { get; private set; }
+        public string Telefonszam { get; private set; }
+        public int Ferohely { get; private set; }
+        public string utvonalak { get; private set; }
+
+        public auto(string szoveg)
+        {
+            string[] a = szoveg.Split(';');
+            Indulas = a[0];
+            Cel = a[1];
+            Rendszam = a[2];
+            Telefonszam = a[3];
+            Ferohely = int.Parse(a[4]);
+            utvonalak = a[0] + "-" + a[1];
+        }
+
+
+    }
+
+
+}
+
---
+++ telekocsiform/igeny.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telekocsiform
+{
+    class igeny
+    {
+        public string Azonosito { get; private set; }
+
+        public string Indulas { get; private set; }
+
+        public string Cel { get; private set; }
+        public int Szemelyek { get; private set; }
+
+        public string utvonalak { get; private set; }
+
+        public igeny(string szoveg)
+        {
+            string[] a = szoveg.Split(';');
+            Azonosito = a[0];
+            Indulas = a[1];
+            Cel = a[2];
+            Szemelyek = int.Parse(a[3]);
+            utvonalak = a[0] + "-" + a[1];
+        }
+
+        public int VanAuto(List<auto> autok)
+        {
+
+
+            int i = 0;
+            while (i < autok.Count && !(utvonalak == autok[i].utvonalak &&
+                Szemelyek <= autok[i].Ferohely))
+            {
+                i++;
+            }
+            if (i < autok.Count)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+}
